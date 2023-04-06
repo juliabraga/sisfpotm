7b721ea563471546bc0dd6f0316cc5e0402c4f78
juliabraga <juliabraga@gmail.com>
2023-04-06 13:43:14 -0300

    test convert

---
+++ itunes-xml-to-json/README.md
@@ -0,0 +1,11 @@
+# Conversor XML para JSON
+
+1 - Rodar 'npm install' para instalar as dependências.
+
+2 - Colocar o arquivo xml com o nome de 'tabela.xml' dentro da pasta raiz(itunes-xml-to-json).
+
+3 - rodar o comando 'npm start'.
+
+4 - vai ser gerado um arquivo 'tabela.json' na pasta frontend/public.
+
+5 - publicar a nova versão do site.
---
+++ itunes-xml-to-json/index.js
@@ -0,0 +1,44 @@
+const parser = require("xml2json");
+const fs = require("fs");
+const moment = require("moment");
+
+moment.locale("pt-br");
+
+let fileInputName = "tabela.xml";
+let fileOutputName = "tabela.json";
+
+function getAlbum(track) {
+  let index = 2;
+  if (track.key.some((k) => k.includes("Album Artist"))) index = index + 1;
+  if (track.key.some((k) => k.includes("Composer"))) index = index + 1;
+  return track.string[index];
+}
+
+async function init() {
+  fs.readFile(fileInputName, "utf8", function (err, fileData) {
+    const json = parser.toJson(fileData, {
+      object: true,
+      reversible: false,
+      coerce: false,
+      sanitize: false,
+      trim: true,
+      arrayNotation: false,
+      alternateTextNode: false,
+    });
+
+    const tracks = json.plist.dict.dict.dict.map((track) => ({
+      interprete: track.string[1],
+      tempo: new Date(+track.integer[2]).toISOString().slice(14, 19),
+      album: getAlbum(track),
+      dataInclusao: moment(track.date[1]).format("DD/MM/YYYY"),
+    }));
+
+    const data = JSON.stringify(tracks);
+    fs.writeFile(fileOutputName, data, (err) => {
+      if (err) throw err;
+      console.log("xml parsed successful");
+    });
+  });
+}
+
+init();
